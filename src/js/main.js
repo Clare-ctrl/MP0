002cfb5c27a8c0d149be383677a8a088c13eeb8d
Mengmeng Fang <mengmengfang@Mengmengs-MacBook-Air.local>
2025-10-05 22:19:22 -0500

    Change and update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
-function changeColor(color) {
-        var target = document.getElementById('card')
-        target.style.backgroundColor = color;
-}
+var target = document.getElementById('card');
+
+document.getElementById('redo').addEventListener('click', function() {
+  target.style.backgroundColor = 'red';
+});
+
+document.getElementById('blueo').addEventListener('click', function() {
+  target.style.backgroundColor = 'blue';
+});
+
+document.getElementById('greyo').addEventListener('click', function() {
+  target.style.backgroundColor = 'grey';
+});
